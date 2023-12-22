745d33eeb7a9f6db81d2613fe3eb347d0bfad142
Seme'e <sam2020tek@gmail.com>
2023-12-21 23:45:06 -0500

    Task 12: Added eurekaclient to services

--- inventory-service/src/main/java/com/backend/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/backend/inventoryservice/InventoryServiceApplication.java
@@ -5,10 +5,12 @@ import com.backend.inventoryservice.Repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
